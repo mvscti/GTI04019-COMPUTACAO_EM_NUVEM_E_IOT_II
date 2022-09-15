a040c35f230ccbd9c86e9e8d3eae7baa40e8b280
marcus@mvscti.com.br <>
2022-09-15 08:17:46 -0300

    Adicionando cliente InfluxDB Python

---
+++ series_temporais/client.py
@@ -0,0 +1,63 @@
+#https://www.influxdata.com/blog/getting-started-python-influxdb/
+
+from influxdb import InfluxDBClient
+
+username=''
+senha=''
+client = InfluxDBClient(host='localhost', port=8086, username=username, password=senha)
+'''Imprimindo a lista de banco de dados'''
+print(client.get_list_database())
+
+'''Cria uma base de dados'''
+client.create_database('exemplo')
+
+'''Seleciona uma base de dados'''
+client.switch_database('exemplo')
+
+''' Pontos de uma 'escova de dentes inteligente'. 
+Cada ponto representa um 'evento de limpeza', cada um deles começando por volta de 8 da manhã,
+idetificados com a tag da pessoa e do id da escova, bem como o tempo (em segundos) que a limpeza durou '''
+json_body = [
+    {
+        "measurement": "brushEvents",
+        "tags": {
+            "user": "Carol",
+            "brushId": "6c89f539-71c6-490d-a28d-6c5d84c0ee2f"
+        },
+        "time": "2018-03-28T8:01:00Z",
+        "fields": {
+            "duration": 127
+        }
+    },
+    {
+        "measurement": "brushEvents",
+        "tags": {
+            "user": "Carol",
+            "brushId": "6c89f539-71c6-490d-a28d-6c5d84c0ee2f"
+        },
+        "time": "2018-03-29T8:04:00Z",
+        "fields": {
+            "duration": 132
+        }
+    },
+    {
+        "measurement": "brushEvents",
+        "tags": {
+            "user": "Carol",
+            "brushId": "6c89f539-71c6-490d-a28d-6c5d84c0ee2f"
+        },
+        "time": "2018-03-30T8:02:00Z",
+        "fields": {
+            "duration": 129
+        }
+    }
+]
+
+''' Inserindo os pontos'''
+client.write_points(json_body)
+
+''' Selecionando'''
+#results=client.query('SELECT "duration" FROM "exemplo"."autogen"."brushEvents" WHERE time > now() - 4d GROUP BY "user"')
+results=client.query('SELECT "duration" FROM "exemplo"."autogen"."brushEvents"  GROUP BY "user"')
+''' Imprimindo o resultado'''
+print(results.raw)
