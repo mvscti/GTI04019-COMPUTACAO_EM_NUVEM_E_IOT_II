5699553de96ffb89dafec44e750a3c0bee144458
marcus@mvscti.com.br <>
2022-09-20 22:47:16 -0300

    Entregando primeira versão funcional do openweather_pandas.py

--- series_temporais/openweather_pandas.py
+++ series_temporais/openweather_pandas.py
@@ -1,30 +1,83 @@
 import requests
-import influxdb_client
 import pandas as pd
-from influxdb_client import InfluxDBClient
-from influxdb_client.client.write_api import SYNCHRONOUS
+from influxdb import InfluxDBClient
+import configparser
+import geocoder
 
-url="localhost:8086"
-username="admin"
-senha="123"
-openWeatherMap_token = ""
+config = configparser.ConfigParser()
+#Lê arquivo com as configurações
+config.read('config.ini')
+host=config['INFLUXDB']['Endereco']
+port=config['INFLUXDB']['Porta']
+username=config['INFLUXDB']['Username']
+senha=config['INFLUXDB']['Senha']
+dbname=config['INFLUXDB']['DB']
+openWeatherMap_token = config['OPENWEATHER']['Key']
 openWeatherMap_lat = "33.44"
 openWeatherMap_lon = "-94.04"
-openWeather_url = "https://api.openweathermap.org/data/3.0/onecall"
-protocol='line'
-# Get time series data from OpenWeatherMap API
-params = {'lat':openWeatherMap_lat, 'lon':openWeatherMap_lon, 'exclude': "minutely,daily", 'appid':openWeatherMap_token}
-r = requests.get(openWeather_url, params = params).json()
-print(r)
-hourly = r['hourly']
+openWeather_url = "https://api.openweathermap.org/data/2.5/forecast"
 
-# Convert data to Pandas DataFrame and convert timestamp to datetime object
-df = pd.json_normalize(hourly)
-df = df.drop(columns=['weather', 'pop'])
-df['dt'] = pd.to_datetime(df['dt'], unit='s')
-print(df.head)
 
-# Write data to InfluxDB
-with InfluxDBClient(host='localhost', port=8086, username=username, password=senha) as client:
-   df = df
-   client.write_points(df, 'tempo', protocol=protocol)
+def defineLocalizacao():
+   print("Definindo sua geolocalização...")
+   '''Descobre as coordenadas geográficas baseadas em seu endereço IP'''
+   return geocoder.ip('me').latlng
+
+def formataDadosProtocoloLinha(data, city_data):
+   json_data = [
+        {
+            "measurement": "weather",
+            "tags": {
+                "city": city_data['name'][0],
+                "location_id": city_data['id'][0]
+            },
+            "time": data['dt'],
+
+            "fields":
+            {
+                "coord_lon": city_data['coord.lon'][0],
+                "coord_lat": city_data['coord.lat'][0],
+                "main_temp":data['main.temp'],
+                "main_pressure":(data['main.pressure']/10),
+                "main_humidity":data['main.humidity'],
+                "main_temp_min":data['main.temp_min'],
+                "main_temp_max":data['main.temp_max'],
+                "visibility":data['visibility'],
+                "wind_speed":data['wind.speed'],
+                "wind_deg":data['wind.deg'],
+                "clouds_all":data['clouds.all'],
+            }
+      }
+   ]
+   return json_data
+
+
+def main():
+   loc=defineLocalizacao()
+   # Recebe dados de séries temporais da API do OpenWeather 
+   params = {'lat':loc[0], 'lon':loc[1], 'exclude': "minutely,current,alerts", 'appid':openWeatherMap_token}
+   print("Consultando a API do OpenWeather")
+   r = requests.get(openWeather_url, params = params).json()
+
+   # Convertendo os dados para um DataFrame pandas
+   df = pd.json_normalize(r.get("list"))
+   #informações da localidade onde a busca ocorreu
+   city_df=pd.json_normalize(r.get("city")) 
+   #excluí colunas informadas
+   df = df.drop(columns=['weather', 'pop'])
+   #converte data em formato Unix para timestamp
+   df['dt'] = pd.to_datetime(df['dt'], unit='s')
+   print("Cabeçalho do DataFrame pandas")
+   print(df.head)
+
+
+   # Grava os pontos no InfluxDB
+   client = InfluxDBClient(host=host, port=port, username=username, password=senha)
+   client.create_database(dbname)
+   client.switch_database(dbname)
+   print("Salvando os dados no InfluxDB")
+   for index, row in df.iterrows():
+      client.write_points(formataDadosProtocoloLinha(row, city_df))
+
+if __name__ == '__main__':
+    main()
